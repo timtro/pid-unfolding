ce8a06ebd87567b67a8e5d103c06d4cb488f09ec
timtro <timtro@gmail.com>
2018-06-22 10:53:06 -0400

    Refactoring.

--- test/utest-numerics-Plant.cpp
+++ test/utest-numerics-Plant.cpp
@@ -6,11 +6,10 @@
 #include "Plant.hpp"
 
 using boost::hana::curry;
-
 namespace odeint = boost::numeric::odeint;
 
 template <typename Stepper>
-auto do_step(Plant p, Stepper stepper, double dt, PState x0) -> PState {
+auto do_step_with(Plant p, Stepper stepper, double dt, PState x0) -> PState {
   stepper.do_step(p, x0, 0., dt);  // time doesn't matter.
   return x0;
 }
@@ -27,20 +26,26 @@ auto step_while(Predicate p, F f, A x0) -> decltype(f(x0)) {
   return current;
 }
 
-TEST_CASE("Study terminal velocity. Starting from rest and the origin …") {
+TEST_CASE("Study terminal velocity. Starting from rest at the origin …") {
   // The plant models a point mass with a static force and a quadratic drag
   // term.
   // (*)  Fs - η v^2 = m dv/dt
   //     where
-  //       * Fs is the static drag force,
+  //       * Fs is the static force,
   //       * η is the drag coefficient,
   //       * m is the inertial mass, and
   //       * v is the linear speed.
   // The terminal velocity is apparent in (*), since dv/dt will be 0:
   // (†) v = (Fs / η)^(1/2)
 
+  constexpr auto do_step = curry<4>(do_step_with<odeint::runge_kutta4<PState>>);
   constexpr double dt = 0.1;
-  const PState x0{0., 0., 0.};
+  constexpr PState x0{0., 0., 0.};
+
+  constexpr double convergenceTolerance = 1E-12;
+  constexpr auto v_diff_not_within_tolerance = [](PState last, PState current) {
+    return (current[1] - last[1]) > convergenceTolerance;
+  };
 
   SECTION(
       "and given (Fs, η) = (4, 1), the terminal velocity should be approx. "
@@ -48,20 +53,9 @@ TEST_CASE("Study terminal velocity. Starting from rest and the origin …") {
     const auto plant = Plant(4., 1.);
     odeint::runge_kutta4<PState> stepper;
 
-    const auto step =
-        curry<4>(do_step<odeint::runge_kutta4<PState>>)(plant, stepper, dt);
-
-    // PState x = x0;
-    // for (double t = 0.; t < 20; t += dt) stepper.do_step(plant, x, t, dt);
-    // REQUIRE(x[1] == Approx(2));
-
-    PState const_v = step_while(
-        [](PState last, PState current) {
-          return (current[1] - last[1]) > 1E-28;
-        },
-        step, x0);
-
-    REQUIRE(const_v[1] == Approx(2));
+    const auto step = do_step(plant, stepper, dt);
+    PState atTerminalVel = step_while(v_diff_not_within_tolerance, step, x0);
+    REQUIRE(atTerminalVel[1] == Approx(2));
   }
 
   SECTION(
@@ -70,15 +64,8 @@ TEST_CASE("Study terminal velocity. Starting from rest and the origin …") {
     const auto plant = Plant(9., 1.);
     odeint::runge_kutta4<PState> stepper;
 
-    const auto step =
-        curry<4>(do_step<odeint::runge_kutta4<PState>>)(plant, stepper, dt);
-
-    PState const_v = step_while(
-        [](PState last, PState current) {
-          return (current[1] - last[1]) > 1E-28;
-        },
-        step, x0);
-
-    REQUIRE(const_v[1] == Approx(3));
+    const auto step = do_step(plant, stepper, dt);
+    PState atTerminalVel = step_while(v_diff_not_within_tolerance, step, x0);
+    REQUIRE(atTerminalVel[1] == Approx(3));
   }
 }
