0f378050a9a6e29ebbc47c2a4510311f96de4dbb
timtro <timtro@gmail.com>
2018-06-25 14:14:14 -0400

    clang-format

--- test/test-util.hpp
+++ test/test-util.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <vector>
 #include <type_traits>
+#include <vector>
 
 namespace util {
 
   template <typename T, typename A, typename F>
-  auto fmap(const std::vector<T, A>& v, F f) {
+  auto fmap(const std::vector<T, A> &v, F f) {
     using MapedToType = std::invoke_result_t<F, decltype(v.at(0))>;
     std::vector<MapedToType> result;
     result.reserve(v.size());
@@ -16,4 +16,4 @@ namespace util {
     return result;
   }
 
-}// namespace util
+}  // namespace util
