da3468a03a48bad1bc56fb187907399127b7929b
timtro <timtro@gmail.com>
2018-06-30 00:14:10 -0400

    Using gnuplot to verify results. TODO: write actual tests based on independent calculations.

--- test/utest-pid-frp.cpp
+++ test/utest-pid-frp.cpp
@@ -1,13 +1,20 @@
+#include <iostream>
+
 #include <sodium/sodium.h>
-#include <Eigen/Dense>
+#include <boost/hana/functional/curry.hpp>
 #include <boost/numeric/odeint.hpp>
 #include <catch/catch.hpp>
-#include <iostream>
+
+#include "../lib/control-frp.hpp"
 #include "../lib/pid.hpp"
 
 #include "Plant.hpp"
+#include "test-util.hpp"
 
-namespace odeint = boost::numeric::odeint;
+#include "gnuplot-iostream.h"
+
+namespace ode = boost::numeric::odeint;
+using boost::hana::curry;
 using Real_t = double;
 using CState = PIDState<Real_t>;
 
@@ -21,57 +28,75 @@ const auto now = chrono::steady_clock::now();
 constexpr double dt = 0.01;  // seconds.
 constexpr auto dts = double_to_duration(dt);
 
-odeint::runge_kutta4<PState> rk4;
+inline SignalPt<double> to_error(const SignalPt<PState>& a, const PState& b) {
+  return {a.time, a.value[0] - b[0]};
+}
+
+// do_step : Plant → Stepper → double → PState → PState
+//                             ^ dt     ^ x[k]   ^ x[k+1]
+constexpr auto do_step = curry<4>(sim::do_step_with<ode::runge_kutta4<PState>>);
 
 TEST_CASE("") {
-  Plant p{0., 0.};
-  CState u0 = {now - dts, 0., 0., 0.};
+  const sodium::cell<PState> setPoint({1., 0., 0.});
+  const sodium::stream_sink<SignalPt<PState>> sPlantState;
+  const auto sError = sPlantState.snapshot(setPoint, &to_error);
 
-  // plantOutput are just perfect state measurements.
-  sodium::stream_sink<SignalPt<PState>> plantOutput;
-  sodium::cell<double> setpoint(0.);
+  ode::runge_kutta4<PState> rk4;
+  ode::runge_kutta4<PState> stepper;
 
-  const auto error = plantOutput.snapshot(
-      setpoint, [](const SignalPt<PState>& x, const double& setpt) {
-        return SignalPt<double>{x.time, setpt - x.value[0]};
-      });
-  auto errorRecord = std::make_shared<std::vector<SignalPt<double>>>();
-  auto error_unlisten =
-      error.listen([errorRecord](auto x) { errorRecord->push_back(x); });
+  const CState u0 = {now - dts, 0., 0., 0.};
+  const sim::Plant plant{-1., 20.};
 
-  const auto control = error.accum<CState>(u0, pid_algebra(1., 1., 1.));
-  auto ctrlScan = std::make_shared<std::vector<CState>>();
-  auto ctrl_unlisten =
-      control.listen([ctrlScan](auto x) { ctrlScan->push_back(x); });
+  // plant_step : PState → PState
+  const auto plant_step = do_step(plant, rk4, dt);
 
-  std::vector<PState> simulation;
+  const auto cControl = ctrl::control_frp(pid_algebra(1., 1., 1.), sError, u0);
+  // Or equivalently,
+  // const auto cControl = sError.accum<CState>(u0, pid_algebra(1., 1., 1.));
+
+  // Listeners, to monitor and test output.
+  auto errorRecord = std::make_shared<std::vector<SignalPt<Real_t>>>();
+  const auto sError_unlisten = sError.listen([errorRecord](auto x) {
+    errorRecord->push_back(x);
+    // std::cout << "     sError(" << errorRecord->size() << "): "
+    //          << " [ " << (x.time - now).count() << " ] {" << x.value << "\n";
+  });
+
+  auto ctrlRecord = std::make_shared<std::vector<CState>>();
+  const auto cControl_unlisten = cControl.listen([ctrlRecord](auto x) {
+    ctrlRecord->push_back(x);
+    // std::cout << "   cControl(" << ctrlRecord->size() << "): "
+    //           << " [ " << (x.time - now).count() << " ] "
+    //           << "{errSum:" << x.errSum << ", error:" << x.error
+    //           << ", ctrlVal:" << x.ctrlVal << "}\n";
+  });
+
+  auto plantRecord = std::make_shared<std::vector<SignalPt<PState>>>();
+  const auto sPlantState_unlisten = sPlantState.listen([plantRecord](auto x) {
+    plantRecord->push_back(x);
+    // std::cout << "sPlantState(" << plantRecord->size() << "): "
+    //           << " [ " << (x.time - now).count() << " ] "
+    //           << "{ x:" << x.value[0] << ", v:" << x.value[1]
+    //           << ", u:" << x.value[2] << "}\n";
+  });
 
   {
     PState x = {0., 0., 0.};
-    simulation.push_back(x);
-    plantOutput.send({now, x});
-    for (double t = dt; t < 1; t += dt) {
-      x[2] = control.sample().ctrlVal;
-      rk4.do_step(p, x, t, dt);
-      simulation.push_back(x);
-      plantOutput.send({now + double_to_duration(t), x});
+    sPlantState.send({now, x});
+    for (int k = 1; k < 10001; ++k) {
+      x[2] = cControl.sample().ctrlVal;
+      rk4.do_step(plant, x, 0, dt);
+      sPlantState.send({now + k * dts, x});
     }
   }
 
-  ctrl_unlisten();
-  error_unlisten();
+  sError_unlisten();
+  cControl_unlisten();
+  sPlantState_unlisten();
 
-  // for (auto& each : simulation)
-  //   std::cout << each[0] << ", " << each[1] << ", " << each[2] << std::endl;
-
-  // for (auto& each : *ctrlScan)
-  //   std::cout << "Time: " << each.time.time_since_epoch().count()
-  //             << " Error: " << each.error
-  //             << " errSum: " << each.errSum
-  //             << " ctrlVal: " << each.ctrlVal << std::endl;
-
-  // for (auto& each: *errorRecord)
-  //   std::cout << "Time: " << each.time.time_since_epoch().count()
-  //             << ", Error: " << each.value
-  //             << std::endl;
+  Gnuplot gp;
+  gp << "plot '-' u 1:2 w l\n";
+  gp.send1d(util::fmap(
+      [](auto x) { return std::make_pair((x.time-now).count(), x.value[0]); },
+      *plantRecord));
 }
