24887fb6d478220e91a75878c5d8ff0af8539954
timtro <timtro@gmail.com>
2018-07-12 15:58:05 -0400

    Forgot the add the new dir like a bonehead.

---
+++ include/util/util-frp.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+
+#include <sodium/sodium.h>
+
+namespace chrono = std::chrono;
+
+namespace util {
+
+  template <typename T>
+  auto make_listener(const sodium::stream<T> &s) {
+    auto sRecord = std::make_shared<std::vector<T>>();
+    const auto s_unlisten =
+        s.listen([sRecord](auto x) { sRecord->push_back(x); });
+    return std::make_pair(s_unlisten, sRecord);
+  }
+
+}  // namespace util
