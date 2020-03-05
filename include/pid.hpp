3e9aaa0c47785bb1fc7464235774de1c255a3b68
timtro <timtro@gmail.com>
2020-03-05 15:49:58 -0500

    Using <chrono> properly now in pid_algebra.

--- include/pid.hpp
+++ include/pid.hpp
@@ -26,16 +26,11 @@ template <typename Clock = chrono::steady_clock>
 auto pid_algebra(double kp, double ki, double kd) {
   return [kp, ki, kd](PIDState<Clock> prev,
                       SignalPt<double, Clock> errSigl) -> PIDState<Clock> {
-    const auto deltaT =
-        chrono::duration_cast<chrono::nanoseconds>(errSigl.time - prev.time)
-            .count();
-    if (deltaT <= 0)
-      return prev;
-    constexpr int nanosecondsPerSecond = 1E9;
-    const auto errSum =
-        prev.errSum + (errSigl.value * deltaT / nanosecondsPerSecond);
-    const auto dErr =
-        (errSigl.value - prev.error) * nanosecondsPerSecond / deltaT;
+    const chrono::duration<double> deltaT =
+        errSigl.time - prev.time;
+    if (deltaT <= chrono::seconds{0}) return prev;
+    const auto errSum = prev.errSum + (errSigl.value * deltaT.count());
+    const auto dErr = (errSigl.value - prev.error) / deltaT.count();
     const auto ctrlVal = kp * errSigl.value + ki * errSum + kd * dErr;
 
     return {errSigl.time, errSum, errSigl.value, ctrlVal};
